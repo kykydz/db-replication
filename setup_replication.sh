a711b0777dad0672ed8951079ec9fd9a6fb78c4a
kykydz <zkyhersa@gmail.com>
2024-11-24 13:20:07 +0700

    fix: move dockerfile and add comment

--- setup_replication.sh
+++ setup_replication.sh
@@ -3,18 +3,21 @@
 echo "Step [1] Starting docker compose..."
 docker compose -f docker-compose.yaml up -d
 
+echo "Restarting server after initial configuration..."
 docker restart db_master_svc db_replica_svc
+echo "Restarted successfully!"
 
 # Wait for 5 seconds
-echo "...waiting for for both server to be ready..."
+echo "Waiting for for both DB server to be ready..."
 sleep 5
 
 echo "Step [2] Grant replication user 'piuw_dev'"
 docker exec -it db_master_svc mysql -uroot -ppass123 -e \
     "GRANT REPLICATION SLAVE ON *.* TO 'piuw_dev'@'%'; \
     FLUSH PRIVILEGES;"
+echo "Granted successfully!"
 
-echo "...waiting for for both server to be ready..."
+echo "Waiting for for both DB server to be ready..."
 sleep 5
 
 # check master status, for: getting which log filename and position, save into OUTPUT
@@ -22,7 +25,7 @@ echo "Step [3] Getting master LOG_FILE and LOG_POSITION..."
 docker exec -i db_master_svc mysql -uroot -ppass123 -e "SHOW MASTER STATUS;" > master_status.txt
 export MASTER_LOG_FILE=$(awk 'NR==2 {print $1}' master_status.txt)
 export MASTER_LOG_POSITION=$(awk 'NR==2 {print $2}' master_status.txt)
-
+echo "Get master status successfully!"
 echo "MASTER_LOG_FILE=$MASTER_LOG_FILE"
 echo "MASTER_LOG_POSITION=$MASTER_LOG_POSITION"
 
@@ -36,6 +39,7 @@ docker exec -it db_replica_svc mysql -uroot -ppass123 -e \
     MASTER_LOG_POS=$MASTER_LOG_POSITION, \
     MASTER_CONNECT_RETRY=60, \
     GET_MASTER_PUBLIC_KEY=1;"
+echo "Changed successfully!"
 
 echo "Step [5] Start replica db server..."
 docker exec -it db_replica_svc mysql -uroot -ppass123 -e "START REPLICA;"
